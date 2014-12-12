3d9aa4b013067050bc8009a0c421c4ec28a0bab5
Tomo Krajina <tkrajina@gmail.com>
2014-12-12 20:10:19 +0100

    init.sh

---
+++ init.sh
@@ -0,0 +1,5 @@
+wget https://raw.githubusercontent.com/tkrajina/dotfiles/master/.tmux.conf
+wget https://raw.githubusercontent.com/tkrajina/dotfiles/master/.vimrc
+sudo apt-get install vim
+sudo apt-get install tmux
+echo "set -o vi" >> .bashrc
