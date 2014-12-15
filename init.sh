7f4d46ce244f74a3df6ac5d6ddd4ee5e7586cced
Tomo Krajina <tkrajina@gmail.com>
2014-12-15 22:10:18 +0100

    export EDITOR

--- init.sh
+++ init.sh
@@ -2,4 +2,6 @@ wget https://raw.githubusercontent.com/tkrajina/dotfiles/master/.tmux.conf
 wget https://raw.githubusercontent.com/tkrajina/dotfiles/master/.vimrc
 sudo apt-get install vim
 sudo apt-get install tmux
-echo "set -o vi" >> .bashrc
+echo "\nCustom:\n" >> .bashrc
+echo "\nset -o vi\n" >> .bashrc
+echo "\nexport EDITOR=vim\n" >> .bashrc
